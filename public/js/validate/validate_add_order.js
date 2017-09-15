f296e4ac62bfbc3a2daf5ca381b52f7caafddda2
phamtuananh1996 <phamtuananh760@gmail.com>
2017-09-15 10:56:55 +0700

    Tuan Anh

--- public/js/validate/validate_add_order.js
+++ public/js/validate/validate_add_order.js
@@ -56,6 +56,7 @@ $(document).ready(function() {
            $('body').scrollTop(0);
          }
        },error: function(res){
+        $('#loading').hide();
           // console.log(res);
         }
       });
@@ -63,6 +64,7 @@ $(document).ready(function() {
     e.stopImmediatePropagation();
   });
   $('#btn_new_orders').click(function(e){
+    $('#loading').show();
     if($('#userifo_form').valid()){
      var company = $('#company').val();
      var name = $('#name_user').val();
@@ -93,6 +95,7 @@ $(document).ready(function() {
       },
       dataType: "json",
       success: function(res){
+        $('#loading').hide();
         if(res.user_id){
           $('#fullname').val(res.fullname);
           $('#user_id').val(res.user_id);
@@ -103,7 +106,7 @@ $(document).ready(function() {
           $('body').scrollTop(0);
         }
       },error: function(res){
-        // console.log(res);
+        $('#loading').hide();
       }
     });
    }
@@ -115,6 +118,7 @@ $(document).ready(function() {
     $('body').scrollTop(0);
   });
   $('#btn_order_apply').click(function(e){
+    $('#loading').show();
     if($('#orders_form').valid()){
       var user_id = $('#user_id').val();
       var name_product = $('#product').val();
@@ -158,12 +162,13 @@ $(document).ready(function() {
         dataType: "json",
         success: function(res){
           if(res.success){
+            $('#loading').hide();
             $('#create_order').remove();
             $('#success_order').show();
             $('body').scrollTop(0);
           }
         },error: function(res){
-          console.log(res);
+          $('#loading').hide();
         }
       });
     }
@@ -395,7 +400,7 @@ $(document).ready(function() {
     else
       return false
 
-  }, 'Lương phải nhỏ hơn 100 triệu');
+  }, 'Giá phải nhỏ hơn 100 triệu');
     $.validator.addMethod("check_pre_pay", function (value, element) {
       if($('#pre_pay').val()==''&&$('#select_rate').val()=='')
         return false
